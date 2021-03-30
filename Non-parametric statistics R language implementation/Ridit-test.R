51b9d02375df134aba0324c1e2d47b6fed4d8386
1-z-1 <2423808173@qq.com>
2021-03-30 17:40:40 +0800

    rename the file of data

--- Non-parametric statistics R language implementation/Ridit-test.R
+++ Non-parametric statistics R language implementation/Ridit-test.R
@@ -1,4 +1,12 @@
-ridit.test = function(x)
+library(foreign)
+camera = read.spss('data gallery/camera-chisq2.sav')
+attach(camera)
+ridit_outlook = rbind(table(c01),table(c02),table(c03),table(c04),table(c05))
+ridit_outlook
+ridit_fun = rbind(table(c11),table(c12),table(c13),table(c14),table(c15),table(c16))
+ridit_fun
+
+ridit.test = function(x)# 这个x应该是一个矩阵列之间应该有强弱关系
 {
   order.num = ncol(x) # 取列数
   treat.num = nrow(x) # 取行数
@@ -25,3 +33,19 @@ ridit.test = function(x)
   pvalue = pchisq(w,treat.num-1,lower.tail = F)
   list(score,confi_inter = confi_inter,W=w,Pvalue = pvalue)
 }
+
+options(digits = 4)
+res_outlook = ridit.test(ridit_outlook)
+graph_outlook = res_outlook$confi_inter
+plot(0,0,ylim=c(0,1),xlim=c(1,5),xlab="outlook",ylab="",
+     main = "Ridit value confidence interval",col="gray7")
+abline(h=0.5)
+for(i in 1:nrow(graph_outlook))
+  lines(c(i,i),graph_outlook[i,],lwd=2)
+res_fun = ridit.test(ridit_fun)
+graph_fun = res_fun$confi_inter
+plot(0,0,ylim = c(0,1),xlim = c(1,6),xlab="function",ylab ="",
+     main = "Ridit value cofidenceinterval",col = "gray7")
+abline(h=0.5)
+for(i in 1:nrow(graph_fun))
+  lines(c(i,i),graph_fun[i,],lwd=2)
